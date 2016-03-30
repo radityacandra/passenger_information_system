7851ffe7b448cbcdcc1d5d734c6fa1a20b6fb9c8
radityacandra <radityacandra@gmail.com>
2016-03-31 06:23:03 +0700

    add login page

---
+++ resources/views/dashboard_login.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+  <script type="text/javascript" src="<?php echo URL::asset('js/jquery-1.12.0.min.js') ?>"></script>
+  <title>Login to Admin Area</title>
+
+  <link rel="stylesheet" href="<?php echo URL::asset('css/dashboard_login.css'); ?>" type="text/css" />
+  <link href="<?php echo URL::asset('css/material_css/bootstrap-material-design.min.css') ?>" type="text/css" rel="stylesheet" />
+  <link href="<?php echo URL::asset('css/material_css/ripples.css') ?>" rel="stylesheet" type="text/css" />
+  <link href="<?php echo URL::asset('css/bootstrap_css/bootstrap.min.css') ?>" rel="stylesheet" type="text/css">
+  <link href="<?php echo URL::asset('css/bootstrap_css/bootstrap-theme.min.css') ?>" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+  <div id="logo" class="container col-md-4 col-md-offset-4">
+    <img src="<?php echo URL::asset('img/logo.gif') ?>" width="150" />
+  </div>
+
+  <div id="container" class="container col-md-4 col-md-offset-4">
+    <div id="header">
+      <h1>Login</h1>
+    </div>
+
+    <div id="content">
+      <form method="post">
+        <div class="form-group label-floating is-empty">
+          <label for="input_email" class="control-label">Alamat Email</label>
+          <input type="email" class="form-control" name="input_email">
+          <span class="help-block">Tolong masukkan email address yang valid</span>
+        </div>
+
+        <div class="form-group label-floating is-empty">
+          <label for="input_password" class="control-label">Password</label>
+          <input type="password" class="form-control" name="input_password">
+        </div>
+
+        <button type="submit" class="btn">submit</button>
+      </form>
+    </div>
+  </div>
+
+  <script type="text/javascript" src="<?php echo URL::asset('js/material_js/material.min.js') ?>"></script>
+  <script type="text/javascript" src="<?php echo URL::asset('js/material_js/ripples.min.js') ?>"></script>
+  <script type="text/javascript" src="<?php echo URL::asset('js/bootstrap_js/bootstrap.min.js') ?>"></script>
+  <script type="text/javascript">
+    $.material.init();
+  </script>
+</body>
+</html>
