4131a5a963d4b18fc07180f38e27a21ab60e3e65
radityacandra <radityacandra@gmail.com>
2016-04-16 09:45:03 +0700

    add deployment task

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -5,4 +5,12 @@
 	git pull origin {{ $branch }}
 	php artisan migrate --force
 	php artisan db:seed --force
+    composer install
+@endtask
+
+@task('deploy_no_seed', ['on' => 'web'])
+    cd /var/www/html/passenger_information_system
+    git pull origin {{ $branch }}
+    php artisan migrate --force
+    composer install
 @endtask
