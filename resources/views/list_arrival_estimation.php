843a229c4f9fd232be25e15fa098cc27e33a10fb
radityacandra <radityacandra@gmail.com>
2016-05-13 14:54:08 +0700

    add styling css to sidebar

--- resources/views/list_arrival_estimation.php
+++ resources/views/list_arrival_estimation.php
@@ -4,13 +4,13 @@
   <script type="text/javascript" src="<?php echo URL::asset('js/jquery-1.12.0.min.js') ?>"></script>
   <title>Semua Jadwal Kedatangan</title>
 
-  <link href="<?php echo URL::asset('css/list_arrival_estimation.css') ?>" type="text/css" rel="stylesheet" />
   <link href="<?php echo URL::asset('css/font-awesome-4.5.0/css/font-awesome.min.css'); ?>" type="text/css"
         rel="stylesheet">
   <link href="<?php echo URL::asset('css/material_css/bootstrap-material-design.min.css') ?>" type="text/css" rel="stylesheet" />
   <link href="<?php echo URL::asset('css/material_css/ripples.css') ?>" rel="stylesheet" type="text/css" />
   <link href="<?php echo URL::asset('css/bootstrap_css/bootstrap.min.css') ?>" rel="stylesheet" type="text/css">
   <link href="<?php echo URL::asset('css/bootstrap_css/bootstrap-theme.min.css') ?>" rel="stylesheet" type="text/css">
+  <link href="<?php echo URL::asset('css/list_arrival_estimation.css') ?>" type="text/css" rel="stylesheet" />
 </head>
 
 <body>
