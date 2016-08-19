0261a040a52b763bfba3fec001bb1fd44a12c7b0
radityacandra <radityacandra@gmail.com>
2016-08-19 10:48:58 +0700

    add route data

--- database/seeds/RouteSeeder.php
+++ database/seeds/RouteSeeder.php
@@ -12,11 +12,50 @@ class RouteSeeder extends Seeder
 	 */
 	public function run()
 	{
+		DB::table('route')->delete();
+
+    DB::unprepared('ALTER TABLE route AUTO_INCREMENT = 1');
+
 		Route::create([
 				'rute_id' => '1A',
 				'created_at'  => \Carbon\Carbon::now(),
 				'updated_at'  => \Carbon\Carbon::now(),
-				'deskripsi'   => "Halte Kusumanegara - Mall Ambarukmo - Jembatan Janti - JEC - Malioboro - Nol Kilometer",
+				'deskripsi'   => "Terminal Prambanan, Bandara Adisutjipto, Stasiun Tugu, Malioboro, JEC",
+		]);
+
+		Route::create([
+				'rute_id' => '1B',
+				'created_at'  => \Carbon\Carbon::now(),
+				'updated_at'  => \Carbon\Carbon::now(),
+				'deskripsi'   => "Bandara Adisutjipto, JEC, km 0, UGM) dan Rute 3B (Terminal Giwangan, UGM, Ringroad Utara, Bandara Adisutjipto, Kotagede",
+		]);
+
+		Route::create([
+				'rute_id' => '2A',
+				'created_at'  => \Carbon\Carbon::now(),
+				'updated_at'  => \Carbon\Carbon::now(),
+				'deskripsi'   => "Terminal Jombor, Malioboro, Kotagede, Kridosono, UGM, Terminal Condongcatur",
+		]);
+
+		Route::create([
+				'rute_id' => '2B',
+				'created_at'  => \Carbon\Carbon::now(),
+				'updated_at'  => \Carbon\Carbon::now(),
+				'deskripsi'   => "Terminal Jombor, Terminal Condongcatur, UGM, Kridosono, Kotagede, km 0",
+		]);
+
+		Route::create([
+				'rute_id' => '3A',
+				'created_at'  => \Carbon\Carbon::now(),
+				'updated_at'  => \Carbon\Carbon::now(),
+				'deskripsi'   => "Terminal Giwangan, Kotagede, Bandara Adisutjipto, Ringroad Utara, UGM, Malioboro",
+		]);
+
+		Route::create([
+				'rute_id' => '3B',
+				'created_at'  => \Carbon\Carbon::now(),
+				'updated_at'  => \Carbon\Carbon::now(),
+				'deskripsi'   => "Terminal Giwangan, UGM, Ringroad Utara, Bandara Adisutjipto, Kotagede",
 		]);
 	}
 }
